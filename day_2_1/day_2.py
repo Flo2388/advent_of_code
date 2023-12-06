4845b1f546b14ad3e4a039e39edf0a2e2a6d0bd7
Florian Friedrich <florian.friedrich@tradebyte.com>
2023-12-06 15:13:09 +0100

    Add first part of day 2

---
+++ day_2_1/day_2.py
@@ -0,0 +1,56 @@
+import re
+
+TOTAL_CUBE_AMOUNT = {"red": 12, "green": 13, "blue": 14}
+
+def parser(line:str) -> list:           #list of dicts
+    round_list = []
+    line = strip_game_id(line)
+    rounds = split_different_rounds(line)
+    for round in rounds:
+        round_list.append(parse_round(round))
+
+    return(round_list)
+
+def parse_round(round:str) -> dict:
+    colour_dict = {}
+    colours = split_colours_per_round(round)
+    for colour in colours:
+        temp_list = colour.split()
+        colour_dict.update( {temp_list[1]: int(temp_list[0])} )
+
+    return colour_dict
+
+def split_colours_per_round(round:str) -> list:         #list of strings
+    return(round.split(sep=","))
+
+def split_different_rounds(line: str) -> list:
+    return(line.split(sep=";"))
+
+def strip_game_id(line: str) -> str:
+    return re.sub(r"Game\s\d+:", "", line).strip()
+
+def is_game_possible(game: list) -> bool:
+    for round in game:
+        if not (round.get("green", 0) <= TOTAL_CUBE_AMOUNT["green"] and round.get("red", 0) <= TOTAL_CUBE_AMOUNT["red"] and round.get("blue", 0) <= TOTAL_CUBE_AMOUNT["blue"]):
+            return False
+
+    return True
+
+def get_game_id(line: str) -> int:
+    game = re.search(r'\w+:', line)
+    game_id = re.search(r'\d+', game.group(0))
+    print(game_id.group(0))
+    return int(game_id.group(0))
+
+def main():
+    id_sum = 0
+    with open("../inputs/day_2_input.txt", "r") as file:
+        for line in file:
+            game = parser(line)
+            if is_game_possible(game):
+                id_sum += get_game_id(line)
+
+    print(id_sum)
+
+if __name__ == "__main__":
+    main()
